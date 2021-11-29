a4d8ff9adf5ada4501cfee667b9fa87e04d2dffd
Peter Lamson <ptrlmsn@gmail.com>
2021-11-29 13:49:29 -0500

    commit tree 6f1b10fac7897841e773f4637abe3bf1c0dcd30b

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+   useEffect(()=>{
+    setTimeout(decreaseTimeRemaining, 1000);
+    return function cleanup() {
+      clearTimeout(setTimeout);
+   }})
+
+   function decreaseTimeRemaining() {
+     setTimeRemaining(timeRemaining - 1)
+     if (timeRemaining === 0) {setTimeRemaining(10); onAnswered(false); 
+    }
+   }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
